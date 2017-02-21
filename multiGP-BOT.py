2e732be15c41853cddc2d9ee8bcae8fd8d8452e9
Unknown <zatgaming@gmail.com>
2017-02-20 23:36:44 -0700

    Done working for the night
    Started to put in Inital DB setup and moved a bunch of stuff to config.

--- multiGP-BOT.py
+++ multiGP-BOT.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 
+import os
 import requests
 import xmltodict
+import sqlite3
+import ConfigParser
 
 
-def main(url):
+def watch_rss(url):
+    # This function will watch the RSS for changes and then will set the change flag to true when it sees them
     responce = requests.get('http://www.multigp.com/multigp/chapter/rss/name/AuroraFPV').content
 
     races_responce = xmltodict.parse(responce, process_namespaces=True)
@@ -27,36 +31,88 @@ def test():
     test = requests.get(test_url)
     print test.status_code
 
+
 def login():
-    login_url = 'http://www.multigp.com/user/site/login/'
-    login_info = {'LoginForm[username]': 'xxx',
-                  'LoginForm[password]': 'xxx',
+    login_url = '{}{}'.format(config.get('multiGP', 'url'), config.get('multiGP', 'login_uri'))
+    login_info = {'LoginForm[username]': config.get('multiGP', 'username'),
+                  'LoginForm[password]': config.get('multiGP', 'password'),
                   'yt0': 'Log in'
                   }
 
-    response = requests.post(login_url, data=login_info)
-    print response.status_code
-    return response
+    response = requests.post(login_url, data=login_info, allow_redirects=False)
+    session.cookies = response.cookies
+
+    return session
 
 
-def click_join(cookies):
+def click_join(session):
+    # Dig out the race ID from the URI
+
+    # Plug the Race ID into the join URI.
+    # Add the aircraft ID to the Data
+    # send the post.
+
+
     event_page = 'http://www.multigp.com/races/view/6443/Whoop-Whoop'
     join_page = 'http://www.multigp.com/races/join/6443'
     # test_cookie =
     form_data = {'RaceEntry[aircraftId]': '16205'}
-    cookie = {'JSESSIONID': 'nd4hv1b8onfq0s9lmuvn67o65vjhtaadc02s4o6qfk6r58nq2jeqakutqu3auqmgsqc5a14e6tksk25o6s4sgo4bnhe4aeulk45kqg3',
-              '03a98afcad68e32d5df5dd91fcf33bae': 'a0b91be0f29ca8804bde7eedff7c97c48417c435s%3A116%3A%22c5f7e3e4de32051fe09f738376f9f8b36adb0c46a%3A4%3A%7Bi%3A0%3Bs%3A4%3A%221722%22%3Bi%3A1%3Bs%3A19%3A%22zatgaming%40gmail.com%22%3Bi%3A2%3Bi%3A2592000%3Bi%3A3%3Ba%3A0%3A%7B%7D%7D%22%3B'}
-    # for c in cookies.cookies:
-    #     print(c.name, c.value)
-    test = requests.post(join_page, cookies=cookie, data=form_data)
+    test = session.post(join_page, data=form_data)
     # pass
 
     print test.status_code
 
 
+def create_db():
+    # This will Create the database and then read the rss feed and input all the existing races into the database.
+    print('Running first time db setup.')
+    with open('./races.db'):
+        pass
+    # Connect to the database
+    conn = sqlite3.connect('./races.db')
+    c = conn.cursor()
+    # Create table
+    c.execute('''CREATE TABLE races (title, link, pubDate, description)''')
+    conn.commit()
+
+    # read rss feed
+    races_responce = session.get(rss_url).content
+
+    # convert to something useful
+    races_responce = xmltodict.parse(races_responce, process_namespaces=True)
+
+    race_data = []
+    for race in races_responce['rss']['channel']['item']:
+        current = []
+        # Dig out the Race ID from the URL.
+        # raceID = race['link'].split('/')
+        current.append(raceID)
+        current.append(race['title'])
+        current.append(race['link'])
+        current.append(race['pubDate'])
+        current.append(race['description'])
+
+
+def start_up():
+    # stuff to run at start up. Does basic check like if the DB has been created or not. If not then create it.
+    if not os.path.exists('./races.db'):
+        create_db()
+
 
 if __name__ == '__main__':
-    url = 'http://www.multigp.com/multigp/chapter/rss/name/AuroraFPV'
-    # main(url)
-    r1 = login()
-    r2 = click_join(r1)
+    config = ConfigParser.RawConfigParser()
+    config.read('./config.ini')
+    rss_url = config.get('chapter', 'url')
+
+    # Flag for if there is a new race not already in the history DB.
+    new_race = False
+
+    #setting up the session
+    session = requests.Session()
+
+    # No need to log in at this point. You can access this page un authenticated
+    watch_rss(rss_url)
+
+    # if new_race:
+    #     login()
+    #     click_join()
