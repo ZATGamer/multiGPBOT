2ee4005b171d4c5a6d285e4d21af1836f94364f7
abarragree <anthony.barragree@charter.com>
2017-02-21 14:39:01 -0700

    Everything should work... (Should being the key word)

--- multiGP-BOT.py
+++ multiGP-BOT.py
@@ -13,11 +13,11 @@ def watch_rss():
     raw_race_data = session.get(rss_url).content
 
     # Convert the XML to something I can use
-    races_responce = xmltodict.parse(raw_race_data, process_namespaces=True)
+    races_response = xmltodict.parse(raw_race_data, process_namespaces=True)
 
     # Create a Dict of all the data using the RaceID as the Key
     race_data = {}
-    for race in races_responce['rss']['channel']['item']:
+    for race in races_response['rss']['channel']['item']:
         race_data[race['link'].split('/')[5]] = {'title': race['title'],
                                                  'link': race['link'],
                                                  'pubDate': race['pubDate'],
@@ -29,34 +29,38 @@ def watch_rss():
         c.execute('''SELECT raceID FROM races WHERE raceID = ?''', (key,))
         db_race = c.fetchone()
 
-        if db_race == None:
-            print("Should Join raceID: {}".format(key))
-
-            join_race(key, race_data[key])
+        if not db_race:
             # if Key Not in DB, fire off the Join function.
+            print("Should Join raceID: {}".format(key))
+            joined = join_race(key, race_data[key])
             # Once successfully joined. Add it to the DB.
-        else:
-            print("raceID {} already Joined".format(key))
+            if joined:
+                add_race_to_db(key, race_data[key])
+            else:
+                print("Something went wrong, Race {} not joined or added to DB.".format(key))
 
 
 def join_race(key, race_data):
-    pass
     # Check to see if race is in blackout
+    in_blackout = check_blackout(race_data)
 
-    in_balckout = check_blackout(race_data)
-    if not in_balckout:
+    if not in_blackout:
         # If not in blackout: Join...
-        # # Log into multiGP
-        # # Send Post to join the race with specified quad.
-        # # Once Joined, add to DB
-        pass
+        # Log into multiGP
+        login()
+
+        # Send Post to join the race with specified quad.
+        join_status = click_join(key)
+
+        if join_status == 200:
+            # Once Joined return True so I know it worked.
+            return True
+        else:
+            return False
+
     else:
         # else don't join but add to DB.
-        pass
-
-
-
-
+        return True
 
 
 def check_blackout(race_data):
@@ -75,8 +79,6 @@ def check_blackout(race_data):
         return False
 
 
-
-
 def login():
     login_url = '{}{}'.format(config.get('multiGP', 'url'), config.get('multiGP', 'login_uri'))
     login_info = {'LoginForm[username]': config.get('multiGP', 'username'),
@@ -87,25 +89,28 @@ def login():
     response = requests.post(login_url, data=login_info, allow_redirects=False)
     session.cookies = response.cookies
 
-    return session
 
+def click_join(race_id):
+    # Build the join URL via config and plug in the raceID.
+    join_url = '{}{}{}'.format(config.get('multiGP', 'url'), config.get('multiGP', 'join_uri'), race_id)
 
-def click_join(session):
-    # Dig out the race ID from the URI
+    # Build the form data
+    aircraft_data = {'RaceEntry[aircraftId]': config.get('chapter', 'aircraft_id')}
+    # using the session send the post.
 
-    # Plug the Race ID into the join URI.
-    # Add the aircraft ID to the Data
-    # send the post.
-
-
-    event_page = 'http://www.multigp.com/races/view/6443/Whoop-Whoop'
-    join_page = 'http://www.multigp.com/races/join/6443'
-    # test_cookie =
-    form_data = {'RaceEntry[aircraftId]': '16205'}
-    test = session.post(join_page, data=form_data)
+    join_post = session.post(join_url, data=aircraft_data)
     # pass
 
-    print test.status_code
+    return join_post.status_code
+
+
+def add_race_to_db(race_id, race_data):
+    # Prep the data for the DB
+    insert_data = (race_id, race_data['title'], race_data['link'], race_data['pubDate'], race_data['description'],
+                   race_data['raceDate'])
+    c.execute(''' INSERT INTO races(raceID, title, link, pubDate, description, raceDate) VALUES(?,?,?,?,?,?)''',
+              insert_data)
+    conn.commit()
 
 
 def create_db():
@@ -114,33 +119,34 @@ def create_db():
     with open('./races.db', 'ab'):
         pass
     # Connect to the database
-    conn = sqlite3.connect('./races.db')
-    c = conn.cursor()
+    db_conn = sqlite3.connect('./races.db')
+    db_c = db_conn.cursor()
     # Create table
-    c.execute('''CREATE TABLE races (raceID, title, link, pubDate, description, raceDate)''')
-    conn.commit()
+    db_c.execute('''CREATE TABLE races (raceID, title, link, pubDate, description, raceDate)''')
+    db_conn.commit()
 
     # read rss feed
-    races_responce = session.get(rss_url).content
+    races_response = session.get(rss_url).content
 
     # convert to something useful
-    races_responce = xmltodict.parse(races_responce, process_namespaces=True)
+    races_response = xmltodict.parse(races_response, process_namespaces=True)
 
     race_data = []
-    for race in races_responce['rss']['channel']['item']:
-        current = []
-        # Dig out the Race ID from the URL.
-        current.append(race['link'].split('/')[5])
-        # current.append(raceID)
-        current.append(race['title'])
-        current.append(race['link'])
-        current.append(race['pubDate'])
-        current.append(race['description'])
-        current.append(race['title'].split('-')[-1])
-        current = tuple(current)
+    for race in races_response['rss']['channel']['item']:
+        current = (
+            # Dig out the Race ID from the URL.
+            race['link'].split('/')[5],
+            # current.append(raceID)
+            race['title'],
+            race['link'],
+            race['pubDate'],
+            race['description'],
+            race['title'].split('-')[-1]
+        )
         race_data.append(current)
 
-    c.executemany(''' INSERT INTO races(raceID, title, link, pubDate, description, raceDate) VALUES(?,?,?,?,?,?)''', race_data)
+    c.executemany(''' INSERT INTO races(raceID, title, link, pubDate, description, raceDate) VALUES(?,?,?,?,?,?)''',
+                  race_data)
     conn.commit()
 
 
@@ -148,21 +154,6 @@ def start_up():
     # stuff to run at start up. Does basic check like if the DB has been created or not. If not then create it.
     if not os.path.exists('./races.db'):
         create_db()
-
-
-def test():
-    conn = sqlite3.connect('./races.db')
-    c = conn.cursor()
-    c.execute('''SELECT raceID FROM races''')
-    raw_races = c.fetchall()
-
-    races = []
-    for thing in raw_races:
-        races.append(thing[0])
-    if '6532' in races:
-        print 'yes'
-
-    print test
 
 
 if __name__ == '__main__':
@@ -173,7 +164,7 @@ if __name__ == '__main__':
     # Flag for if there is a new race not already in the history DB.
     new_race = False
 
-    #setting up the session
+    # setting up the session
     session = requests.Session()
     start_up()
 
@@ -181,10 +172,3 @@ if __name__ == '__main__':
     c = conn.cursor()
 
     watch_rss()
-    # test()
-    # No need to login at this point. You can access this page un authenticated
-    # watch_rss(rss_url)
-
-    # if new_race:
-    #     login()
-    #     click_join()
