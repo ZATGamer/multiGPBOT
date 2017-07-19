1aa034aba74db69a046a66696d5a2f4c3fb99bc3
abarragree <anthony.barragree@charter.com>
2017-07-19 09:40:00 -0600

    update db script

---
+++ update_db.py
@@ -0,0 +1,13 @@
+import ConfigParser
+import sqlite3
+
+if __name__ == '__main__':
+    config = ConfigParser.RawConfigParser()
+    config.read('./config.ini')
+
+    db_path = config.get('database', 'auto_close')
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+
+    c.execute('''ALTER TABLE races ADD COLUMN title''')
+    conn.commit()
